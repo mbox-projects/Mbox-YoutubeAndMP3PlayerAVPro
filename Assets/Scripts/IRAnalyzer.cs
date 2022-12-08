0b5b68889630c355c48d29e795aeb85c8ef7933e
Jeong Yeong Choi <cjsdks128@naver.com>
2022-12-08 20:14:42 +0900

    초기반사음 벽 반사 패닝 시스템 1차 설계

--- Assets/Scripts/IRAnalyzer.cs
+++ Assets/Scripts/IRAnalyzer.cs
@@ -16,16 +16,22 @@ public class IRAnalyzer : MonoBehaviour
     public AudioClip IR = null;
     public int TargetChannel = 0;
     public bool ReverbOn = true;
+
     [Range(-80, 40)]
-    public float ReverbLevel = 0;
+    public float ReverbLevel;// = 0;
+
     [Range(-80, 0)]
-    public float EarlyReflectionLevel = -40;
+    public float EarlyReflectionLevel;// = -40;
+
     [Range(1, 3)]
-    public int FreqResolution = 1;
+    public int FreqResolution;// = 1;
+
     [Range(1, 2)]
-    public float TimeResolution = 1;
-    [Range(0, 360)]
-    public float RayTracingResolution = 16;
+    public float TimeResolution;// = 1;
+
+    [Range(1, 6)]
+    public float RayTracingResolution;// = 16;
+
     public bool DrawRayTracingLine = true;
     float[] IRdata = null;
     float[][] IRArray = null;
@@ -323,8 +329,6 @@ public class IRAnalyzer : MonoBehaviour
         int VerticalVectorN = 360;
         int HorizontalVectorN = 360;
         float SumDistance = 0;
-        float NearWall = 0;
-        Vector3 WallVectorSum = new Vector3();
         float StartTheta = 0;
         float EndTheta = 360;
         float StartPhi = 0;
@@ -333,8 +337,14 @@ public class IRAnalyzer : MonoBehaviour
         GameObject Listener = GameObject.Find("Sphere1");
         Vector3 pos = Speaker.transform.position;
         Vector3 rot = Speaker.transform.eulerAngles;
-        float VerticalDivision = 360f / RayTracingResolution;
-        float HorizontalDivision = 360f / RayTracingResolution;
+        float RayTracingResolution2Power = Mathf.Pow(2, RayTracingResolution);
+
+        float VerticalDivision = 360f / RayTracingResolution2Power;
+        float HorizontalDivision = 360f / RayTracingResolution2Power;
+
+        float LeftEarER = 0;
+        float RightEarER = 0;
+
         for (float i = StartPhi; i < EndPhi; i += HorizontalDivision)
         {
             for (float j = StartTheta; j < EndTheta; j += VerticalDivision)
@@ -347,48 +357,59 @@ public class IRAnalyzer : MonoBehaviour
                     float Theta360 = (Theta / 360f);
                     float Phi360 = (Phi / 360f);
                     const int R = 100;
-                    float ThetaAngle = Pi2 * (rot.x / 360f);
-                    float PhiAngle = Pi2 * (rot.y / 360f);
                     float Pi2Theta360 = Pi2 * Theta360;
                     float Pi2Phi360 = Pi2 * Phi360;
-                    float X = R * Mathf.Sin(ThetaAngle + Pi2Theta360) * Mathf.Cos(PhiAngle + Pi2Phi360);
-                    float Z = R * Mathf.Sin(ThetaAngle + Pi2Theta360) * Mathf.Sin(PhiAngle + Pi2Phi360);
-                    float Y = R * Mathf.Cos(ThetaAngle + Pi2Theta360);
+                    float Z = R * Mathf.Sin(Pi2Theta360) * Mathf.Cos(Pi2Phi360);
+                    float X = R * Mathf.Sin(Pi2Theta360) * Mathf.Sin(Pi2Phi360);
+                    float Y = R * Mathf.Cos(Pi2Theta360);
                     Physics.Raycast(pos, new Vector3(X, Y, Z), out RaycastHit hit);
-                    float SpeakerToWallDistance = Vector3.Magnitude(pos - hit.point);
-                    float WallToListenerDistance = Vector3.Magnitude(hit.point - Listener.transform.position);
-                    float DirectSoundDistance = Vector3.Magnitude(Speaker.transform.position - Listener.transform.position);
+                    float SpeakerToWallDistance = Vector3.Magnitude(hit.point - pos);
+                    Vector3 ListenerPos = Listener.transform.position;
+                    float WallToListenerDistance = Vector3.Magnitude(ListenerPos - hit.point);
+                    float DirectSoundDistance = Vector3.Magnitude(ListenerPos - Speaker.transform.position);
                     float SWD = SpeakerToWallDistance + WallToListenerDistance - DirectSoundDistance;
-                    if (SWD < 340 * 0.05f)
+                   
+                    if (SWD < (340 * 0.05f) && WallToListenerDistance < 2)
                     {
-                        NearWall += WallToListenerDistance;
-                        WallVectorSum += hit.point - Listener.transform.position;
+                        Vector2 LeftEar = new Vector2((-Listener.transform.right).x,(-Listener.transform.right).z);
+                        Vector2 RightEar = new Vector2(Listener.transform.right.x, Listener.transform.right.z);
+
+                        Vector2 B = new Vector2(hit.point.x,hit.point.z);
+                        float left = LeftEar.magnitude;
+                        float right = RightEar.magnitude;
+                        float wall = B.magnitude;
+                        float LeftVol = Vector2.Dot(LeftEar, B) / (left * wall);
+                        float RightVol = Vector2.Dot(RightEar, B) / (right * wall);
+                        LeftEarER += Mathf.Max(LeftVol, 0);
+                        RightEarER += Mathf.Max(RightVol, 0);
                         NumberOfERVector++;
+                        Debug.DrawRay(pos, hit.point - pos, Color.red);
+                        Debug.DrawRay(hit.point, ListenerPos - hit.point, Color.magenta);
                     }
                     SumDistance += Mathf.Abs(SWD);
                     if (DrawRayTracingLine)
                     {
-                        Debug.DrawRay(pos, hit.point - pos, Color.red);
-                        Debug.DrawRay(hit.point, Listener.transform.position - hit.point, Color.magenta);
+                        
                     }
                 }
             }
         }
         float VH = VerticalVectorN * HorizontalVectorN;
         SumDistance /= VH;
-        NearWall /= NumberOfERVector;
-        WallVectorSum /= NumberOfERVector;
-        Vector3 ListenRotation = new Vector3(WallVectorSum.x*Mathf.Sin(Listener.transform.eulerAngles.y),WallVectorSum.y,WallVectorSum.y*Mathf.Cos(Listener.transform.eulerAngles.y));
-        //Debug.Log(string.Format("{0}ms : {1}m", ((SumDistance / 340) * 1000).ToString(), SumDistance));
-        Debug.Log(ListenRotation);
         AudioMixer audioMixer = KsoriAudioSource.ReverbEffectMixer;
         for (int i = 0; i < 9; i++)
         {
             float freq = 31.25f * Mathf.Pow(2, i);
             audioMixer.SetFloat(freq.ToString() + "Hz Predelay", (SumDistance / 340f));
         }
-        //audioMixer.SetFloat("ER Left", WallVectorSum.x);
-        //audioMixer.SetFloat("ER Right", WallVectorSum.x);
+        LeftEarER /= NumberOfERVector;
+        RightEarER /= NumberOfERVector;
+        audioMixer.SetFloat("Left ER", (1+LeftEarER) / 2);
+        audioMixer.SetFloat("Right ER", (1+RightEarER) / 2);
+        Debug.Log(string.Format("{0} {1}",LeftEarER,RightEarER));
+        Debug.DrawRay(Listener.transform.position, -Listener.transform.right, Color.green);
+        Debug.DrawRay(Listener.transform.position, Listener.transform.right, Color.red);
+        Debug.DrawRay(Listener.transform.position, Listener.transform.forward, Color.yellow);
     }
     // Update is called once per frame
     void Update()
